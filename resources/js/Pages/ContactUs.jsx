b3e4a317841892527a1bc65dd8ba513de0b92fce
rudrakumar67 <acharyashivkumar67@gmail.com>
2026-04-22 17:28:43 +0530

    changes

--- resources/js/Pages/ContactUs.jsx
+++ resources/js/Pages/ContactUs.jsx
@@ -10,33 +10,32 @@ export default function ContactUs() {
                 <div className="w-full max-w-7xl mx-auto lg:px-8 grid md:grid-cols-2 gap-12 mb-12 items-center">
                     {/* LEFT SIDE - DETAILS */}
                     <div>
-                        <h2 className="font-bebas text-5xl text-primary mb-4">
+                        <h2 className="text-4xl md:text-4xl lg:text-7xl font-light leading-tight mb-6 opacity-0 animate-[fadeInUp_0.5s_ease-out_0.5s_forwards]">
                         Contact Us
                         </h2>
-
-                        <p className="text-2xl mb-6 font-semibold">
+                        <p className="text-2xl mb-6 font-semibold opacity-0 animate-[fadeInUp_0.5s_ease-out_0.6s_forwards]">
                         Let's discuss your vision.
                         </p>
-                        <p className="mb-8 text-black font-semibold">
+                        <p className="mb-8 text-black font-semibold opacity-0 animate-[fadeInUp_0.5s_ease-out_0.7s_forwards]">
                         Tell us a little about yourself and your dream home. Our expert team
                         will get back to you as soon as possible.
                         </p>
                         <div className="space-y-2">
-                        <div>
+                        <div className="opacity-0 animate-[fadeInUp_0.5s_ease-out_0.8s_forwards]">
                             <h4 className="font-bebas text-xl text-primary">Phone : <a href="tel:+919600663238" className="text-black">+91 9600663238</a></h4>
                         </div>
-                        <div>
+                        <div className="opacity-0 animate-[fadeInUp_0.5s_ease-out_0.9s_forwards]">
                             <h4 className="font-bebas text-xl text-primary">Email : <a href="mailto:info@petras.in" className="text-black">info@petras.in</a></h4>
                         </div>
-                        <div>
-                            <h4 className="font-bebas text-xl text-primary">Follow Us : </h4>
+                        <div className="opacity-0 animate-[fadeInUp_0.5s_ease-out_1.0s_forwards]">
+                            <h4 className="font-bebas text-xl text-primary ">Follow Us : </h4>
                             <a href="https://wa.me/919600663238" className="font-bebas text-xl text-black mr-4" target="_blank"><FontAwesomeIcon icon={faWhatsapp} /></a>
                             <a href="https://www.facebook.com/petrasbuilders/" className="font-bebas text-xl text-black mr-4" target="_blank"><FontAwesomeIcon icon={faFacebook} /></a>
                             <a href="https://www.instagram.com/petrasbuilders/" className="font-bebas text-xl text-black mr-4" target="_blank"><FontAwesomeIcon icon={faInstagram} /></a>
                             <a href="https://www.linkedin.com/company/petrasbuilders/" className="font-bebas text-xl text-black mr-4" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
                         </div>
-                        <div>
-                            <h4 className="font-bebas text-xl text-primary">Address : <a href="https://maps.app.goo.gl/22q45e9Y3y5627qG7" className="text-black">99A, Velachery – Tambaram Main Rd,<br />
+                        <div className="opacity-0 animate-[fadeInUp_0.5s_ease-out_1.1s_forwards]">
+                            <h4 className="font-bebas text-xl text-primary ">Address : <a href="https://maps.app.goo.gl/22q45e9Y3y5627qG7" className="text-black">99A, Velachery – Tambaram Main Rd,<br />
                             Sembakkam, Chennai, Tamil Nadu 600073</a></h4>
                         </div>
 
@@ -44,9 +43,9 @@ export default function ContactUs() {
                     </div>
 
                     {/* RIGHT SIDE - FORM */}
-                    <div className="bg-white p-8 rounded-2xl shadow-lg">
+                    <div className="bg-white p-8 rounded-2xl shadow-lg opacity-0 animate-[fadeInUp_0.5s_ease-out_1.2s_forwards]">
 
-                        <h3 className="font-bebas text-3xl text-primary mb-6"> Send a Message </h3>
+                        <h3 className="text-3xl mb-6 opacity-0 animate-[fadeInUp_0.5s_ease-out_1.3s_forwards]"> Send a Message </h3>
                         <form className="space-y-4">
                             <input
                                 type="text"
@@ -77,7 +76,7 @@ export default function ContactUs() {
                         </form> 
                     </div>
                 </div>
-                <div className="w-full max-w-7xl mx-auto lg:px-8">
+                <div className="w-full max-w-7xl mx-auto lg:px-8 opacity-0 animate-[fadeInUp_0.5s_ease-out_1.4s_forwards]">
                     <iframe 
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.7675674914667!2d80.1616007!3d12.922655100000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525ee94f7fbb29%3A0xf770ab101dd7b9d9!2sPetras%20Solutions%20Private%20Limited!5e0!3m2!1sen!2sin!4v1775819388937!5m2!1sen!2sin" 
                         width="100%" 
